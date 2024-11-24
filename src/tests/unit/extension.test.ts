b6742ac4f624c944f90192e379a3f2497e796e33
Davide Canton <davide.canton5@gmail.com>
2024-11-24 15:17:49 +0100

    updates

--- src/tests/unit/extension.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from 'assert';
-
-import { buildUri } from '../../utils';
-
-suite('Build uri', () => {
-    test('Sample test2', () => {
-        assert.equal(buildUri('a', 'b'), 'a/b');
-    });
-});
